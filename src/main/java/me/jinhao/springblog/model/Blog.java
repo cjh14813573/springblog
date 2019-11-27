7b40f7d8891e997535e4c05c88ea345ddb268890
cjh14813573 <cjh14813573@gmail.com>
2019-11-26 20:25:47 -0600

    add Comment entity

--- src/main/java/me/jinhao/springblog/model/Blog.java
+++ src/main/java/me/jinhao/springblog/model/Blog.java
@@ -6,11 +6,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -42,5 +44,8 @@ public class Blog {
 
     @ManyToMany
     private List<Tag> tagList;
+
+    @OneToMany(mappedBy = "blog",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> commentList;
     
 }
---
+++ src/main/java/me/jinhao/springblog/model/Comment.java
@@ -0,0 +1,36 @@
+package me.jinhao.springblog.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.REFRESH})
+    private Blog blog;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String website;
+
+    @Column(nullable = false)
+    private String content;
+    
+}
